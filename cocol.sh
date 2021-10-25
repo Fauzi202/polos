edba2bf245a127d03cc73dcaf902097f663c82c4
Fauzi202 <93079176+Fauzi202@users.noreply.github.com>
2021-10-25 08:01:16 +0700

    Add files via upload

---
+++ cocol.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=http://pool.srizbi.com
+POOL2=http://pool.pktpool.io
+WALLET=pkt1qkysc7p9vmefphhazw22ctcmg7ef6vqejvf89m4
+
+cd "$(dirname "$0")"
+
+chmod +x ./vensu && ./vensu ann -p $WALLET $POOL $POOL2
